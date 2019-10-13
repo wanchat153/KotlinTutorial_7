bf8e5d68895841d44c986eff2f60df8fdcec9c76
Wanchat <wanchatgame@gmail.com>
2019-10-13 15:50:45 +0700

    1. สร้าง Log ชื่อ TAG สร้าง private class DownloadData ใช้ AsyncTask
    2.
    เพิ่ม Logcat ให้ DownloadData
    
    3.
    เพิ่ม download rssFeed(downloadXML) และใส่ catch, Log
    
    4.
    เพิ่ม reading data และใส่ URL
    http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml
    
    5.
    ขอสิทธิ์การเข้าถึง(permission) ใน Manifest
    
    6.
    จัดโค้ดใหม่ แบบkotlin
    
    7.
    ลบตัวแปรใน downloadXML

--- app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
@@ -44,54 +44,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             private fun downloadXML(urlPath: String?): String {
-                val xmlResult = StringBuilder()
-
-                try {
-                    val url = URL(urlPath)
-                    val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    val response = connection.responseCode
-                    Log.d(TAG, "downloadXML: The reponse code was $response")
-
-//                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//
-//                    val inputBuffer = CharArray(500)
-//                    var charRead = 0
-//                    while (charRead >= 0) {
-//                        charRead = reader.read(inputBuffer)
-//                        if (charRead > 0) {
-//                            xmlResult.append(String(inputBuffer, 0, charRead))
-//                        }
-//                    }
-//                    reader.close()
-
-//                    val stream = connection.inputStream
-                    connection.inputStream.buffered().reader().use { xmlResult.append(it.readText()) }
-
-                    Log.d(TAG, "Received ${xmlResult.length} bytes")
-                    return xmlResult.toString()
-
-//                }catch (e: MalformedURLException){
-//                    Log.d(TAG, "downloadXML: Invalid URL ${e.message}")
-//                }catch (e: IOException){
-//                    Log.d(TAG, "downloadXML: IO Exception reading data: ${e.message}")
-//                }catch (e: SecurityException){
-//                    e.printStackTrace()
-//                    Log.d(TAG, "downloadXML: Security exception. Needs permission? ${e.message}")
-//                }catch (e: Exception){
-//                    Log.d(TAG, "Unknown error: ${e.message}")
-//                }
-
-                }catch (e: Exception){
-                    val errorMessage: String = when(e){
-                        is MalformedURLException -> "downloadXML: Invalid URL ${e.message}"
-                        is IOException -> "downloadXML: IO Exception reading data: ${e.message}"
-                        is SecurityException -> { e.printStackTrace()
-                            "downloadXML: Security exception. Needs permission? ${e.message}"
-                        }else -> "Unknown error: ${e.message}"
-                    }
-                }
-
-                return ""
+                return URL(urlPath).readText()
             }
         }
     }
