69c5bd54328c5d2a388ec6d0b7798367a9e251d9
Wanchat <wanchatgame@gmail.com>
2019-10-13 18:34:41 +0700

    1. สร้าง Log ชื่อ TAG สร้าง private class DownloadData ใช้ AsyncTask
    2.
    เพิ่ม Logcat ให้ DownloadData
    
    3.
    เพิ่ม download rssFeed(downloadXML) และใส่ catch, Log
    
    4.
    เพิ่ม reading data และใส่ URL
    http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml
    
    5.
    ขอสิทธิ์การเข้าถึง(permission) ใน Manifest
    
    6.
    จัดโค้ดใหม่ แบบkotlin
    
    7.
    ลบตัวแปรใน downloadXML
    
    8.
    สร้าง class ParseApplications และ Log
    
    9.
    สร้าง Xml Parse Function
    
    10.
    สร้าง list_item
    
    11.
    สร้าง Array Adapter
    
    12.
    สร้าง onDestroy และ Asynctask
    
    13.
    สร้าง layout list_record
    
    14.
    สร้าง FeedAdapter.kt แล้วทำการ Adapter
    
    15.
    improving Adapter
    
    16.
    ประกาศและใช้ ViewHolder
    
    17.
    สร้าง menu ใน res
    
    18.
    เพิ่ม menu code

--- app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -35,21 +37,48 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
-    private val downloadData by lazy { DownloadData(this, xmlListView) }
+    private var downloadData: DownloadData? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate called")
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=200/xml")
+        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
+    }
 
+    private fun downloadUrl(feedUrl: String){
+        Log.d(TAG, "downloadUrl startiang AsyncTask")
+        downloadData = DownloadData(this, xmlListView)
+        downloadData?.execute(feedUrl)
+        Log.d(TAG, "downloadUrl done")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val feedUrl: String
+
+        when(item.itemId){
+            R.id.mnuFree ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml"
+            R.id.mnuPaid ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=10/xml"
+            R.id.mnuSongs ->
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml"
+            else ->
+                return super.onOptionsItemSelected(item)
+        }
+        downloadUrl(feedUrl)
+        return true
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        downloadData.cancel(true)
+        downloadData?.cancel(true)
     }
 
     companion object {
