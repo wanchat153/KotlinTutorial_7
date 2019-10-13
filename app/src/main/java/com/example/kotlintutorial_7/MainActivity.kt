9d1c2d33eebcbb9ea6ca6ac22e61d79733be5cca
Wanchat <wanchatgame@gmail.com>
2019-10-13 15:06:27 +0700

    1. สร้าง Log ชื่อ TAG สร้าง private class DownloadData ใช้ AsyncTask
    2.
    เพิ่ม Logcat ให้ DownloadData
    
    3.
    เพิ่ม download rssFeed(downloadXML) และใส่ catch, Log

--- app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.security.cert.CertPath
+import kotlin.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,10 +38,34 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
-            override fun doInBackground(vararg p0: String?): String {
-                Log.d(TAG, "doInBackground: starts with ${p0[0]}")
-                return "doInBackground completed"
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML{url[0]}
+                if (rssFeed.isEmpty()){
+                    Log.d(TAG, "doInBackground: Error Downloading")
+                }
+                return rssFeed
             }
         }
+    }
+
+    private fun downloadXML(urlPath: String?): String {
+        val xmlResult = StringBuilder()
+
+        try {
+            val url = URL(urlPath)
+            val connection: HttpURLConnection = url.openConnection()as HttpURLConnection
+            val response = connection.responseCode
+            Log.d(TAG, "downloadXML: The reponse code was $response")
+
+            val inputStream = connection.inputStream
+            val inputStreamReader = InputStreamReader(inputStream)
+            val reader = BufferedReader(inputStreamReader)
+        }catch (e: MalformedURLException){
+            Log.d(TAG, "downloadXML: Invalid URL ${e.message}")
+        }catch (e: IOException){
+            Log.d(TAG, "downloadXML: IO Exception reading data: ${e.message}")
+        }catch (e: Exception){
+            Log.d(TAG, "Unknown error: ${e.message}")
     }
 }
