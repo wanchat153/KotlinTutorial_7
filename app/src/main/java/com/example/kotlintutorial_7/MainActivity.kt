8a0a04cd645cbd51e2b22ec8982dd730ffb000ed
Wanchat <wanchatgame@gmail.com>
2019-10-13 14:40:13 +0700

    1. สร้าง Log ชื่อ TAG สร้าง private class DownloadData ใช้ AsyncTask

--- app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_7/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.kotlintutorial_7
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called")
+
+    }
+
+    private inner class  DownloadData : AsyncTask<String, Void, String>(){
+        private val TAG = "DownloadData"
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 }
